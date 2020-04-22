e17e099afb9f581c2f3e317d509ce29b680b6cf6
Liuba <liuba.kalyta@gmail.com>
2020-04-22 18:52:41 +0300

    MA-5414 - fix bug with lib

--- src/helpers.php
+++ src/helpers.php
@@ -4,16 +4,16 @@
  * @copyright Copyright (c) MISTER.AM, LLC
  * @author    Liuba Kalyta <kalyta@dotsplatform.com>
  */
-if ( ! function_exists('config_path'))
-{
-    /**
-     * Get the configuration path.
-     *
-     * @param  string $path
-     * @return string
-     */
-    function config_path($path = '')
-    {
-        return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
-    }
-}
+//if ( ! function_exists('config_path'))
+//{
+//    /**
+//     * Get the configuration path.
+//     *
+//     * @param  string $path
+//     * @return string
+//     */
+//    function config_path($path = '')
+//    {
+//        return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
+//    }
+//}
