ac030995873574a8327a6bf487e6b7460d856ccb
Liuba <liuba.kalyta@gmail.com>
2020-04-23 10:27:57 +0300

    MA-5414 - fix bug with lib

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,11 +7,17 @@
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/cashback.php', 'cashback'
+        );
+    }
+
     public function boot()
     {
         $this->publishes([
             __DIR__.'/config/cashback.php' => config_path('cashback.php'),
         ]);
     }
-
 }
