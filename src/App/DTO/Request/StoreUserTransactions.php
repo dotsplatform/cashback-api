02d9f120cd9959ff83e3ea738ef3ff73fbde69ca
Oleksandr Polosmak <olexandr.polosmak123@gmail.com>
2023-07-21 16:29:02 +0300

    DEV-15723 changed createTransaction param from dto to list

---
+++ src/App/DTO/Request/StoreUserTransactions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Description of StoreUserTransactions.php
+ * @copyright Copyright (c) DOTSPLATFORM, LLC
+ * @author    Oleksandr Polosmak <o.polosmak@dotsplatform.com>
+ */
+
+namespace Dotsplatform\CashbackApi\DTO\Request;
+
+
+use Illuminate\Support\Collection;
+
+class StoreUserTransactions extends Collection
+{
+    public static function fromArray(array $data): static
+    {
+        return new static(array_map(
+                fn(array $item) => StoreTransactionDTO::fromArray($item),
+                $data,
+            )
+        );
+    }
+}
