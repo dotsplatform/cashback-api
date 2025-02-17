7d83c4afae24af6fc24572d94ead44cbb03e486a
Bohdan Mamontov <bengemingg@gmail.com>
2025-02-17 15:43:42 +0200

    Update Transaction types

--- src/App/DTO/Response/Syrve/Consts/TransactionType.php
+++ src/App/DTO/Response/Syrve/Consts/TransactionType.php
@@ -9,5 +9,20 @@ namespace Dotsplatform\CashbackApi\DTO\Response\Syrve\Consts;
 
 enum TransactionType: string
 {
+    case REFILL_WALLET = 'RefillWallet';
+    case PAY_FROM_WALLET = 'PayFromWallet';
+    case CANCEL_PAY_FROM_WALLET = 'CancelPayFromWallet';
     case REFILL_WALLET_FROM_ORDER = 'RefillWalletFromOrder';
+    case CANCEL_REFILL_WALLET_FROM_ORDER = 'CancelRefillWalletFromOrder';
+
+    public function isCustomerBonusBalanceChangedTransactionType(): bool
+    {
+        return in_array($this, [
+            self::REFILL_WALLET,
+            self::PAY_FROM_WALLET,
+            self::CANCEL_PAY_FROM_WALLET,
+            self::REFILL_WALLET_FROM_ORDER,
+            self::CANCEL_REFILL_WALLET_FROM_ORDER,
+        ]);
+    }
 }
