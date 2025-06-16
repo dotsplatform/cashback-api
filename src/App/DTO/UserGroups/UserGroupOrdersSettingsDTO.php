0b58aa045673dcb19636525fc83236e5aacd1347
Bohdan Mamontov <bengemingg@gmail.com>
2025-06-16 11:55:37 +0300

    Add orders cashback receiving type

--- src/App/DTO/UserGroups/UserGroupOrdersSettingsDTO.php
+++ src/App/DTO/UserGroups/UserGroupOrdersSettingsDTO.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Description of OrdersSettingsDTO.php
+ * Description of UserGroupOrdersSettingsDTO.php
  * @copyright Copyright (c) DOTSPLATFORM, LLC
  * @author    Bogdan Mamontov <bohdan.mamontov@dotsplatform.com>
  */
@@ -8,10 +8,13 @@
 namespace Dotsplatform\CashbackApi\DTO\UserGroups;
 
 use Dots\Data\DTO;
+use Dotsplatform\CashbackApi\DTO\OrdersCashbackReceivingType;
 
 class UserGroupOrdersSettingsDTO extends DTO
 {
     protected bool $ordersCashbackAvailable = false;
+
+    protected string $ordersCashbackReceivingType = OrdersCashbackReceivingType::DELIVERY_TYPE->value;
 
     protected float $cashBackPercentDelivery = 0;
 
@@ -23,9 +26,30 @@ class UserGroupOrdersSettingsDTO extends DTO
 
     protected float $cashBackPercentDeliveryInnerToDoor = 0;
 
+    protected float $cashBackPercentIOS = 0;
+
+    protected float $cashBackPercentAndroid = 0;
+
+    protected float $cashBackPercentWeb = 0;
+
+    protected float $cashBackPercentByCompany = 0;
+
+    protected float $cashBackPercentOther = 0;
+
+    protected float $cashBackPercentOnline = 0;
+
+    protected float $cashBackPercentCash = 0;
+
+    protected float $cashBackPercentTerminal = 0;
+
     public function isOrdersCashbackAvailable(): bool
     {
         return $this->ordersCashbackAvailable;
+    }
+
+    public function getOrdersCashbackReceivingType(): string
+    {
+        return $this->ordersCashbackReceivingType;
     }
 
     public function getCashBackPercentDelivery(): float
@@ -51,5 +75,45 @@ class UserGroupOrdersSettingsDTO extends DTO
     public function getCashBackPercentDeliveryInnerToDoor(): float
     {
         return $this->cashBackPercentDeliveryInnerToDoor;
+    }
+
+    public function getCashBackPercentIOS(): float
+    {
+        return $this->cashBackPercentIOS;
+    }
+
+    public function getCashBackPercentAndroid(): float
+    {
+        return $this->cashBackPercentAndroid;
+    }
+
+    public function getCashBackPercentWeb(): float
+    {
+        return $this->cashBackPercentWeb;
+    }
+
+    public function getCashBackPercentByCompany(): float
+    {
+        return $this->cashBackPercentByCompany;
+    }
+
+    public function getCashBackPercentOther(): float
+    {
+        return $this->cashBackPercentOther;
+    }
+
+    public function getCashBackPercentOnline(): float
+    {
+        return $this->cashBackPercentOnline;
+    }
+
+    public function getCashBackPercentCash(): float
+    {
+        return $this->cashBackPercentCash;
+    }
+
+    public function getCashBackPercentTerminal(): float
+    {
+        return $this->cashBackPercentTerminal;
     }
 }
