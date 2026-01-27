b0a806607b8e9f150fa29d6ab46b63f4e192922a
Bohdan Mamontov <bengemingg@gmail.com>
2026-01-27 13:54:56 +0200

    UPdate

---
+++ src/App/DTO/Request/Transactions/TransactionsStatisticsDTO.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Description of TransactionsStatisticsDTO.php
+ * @copyright Copyright (c) MISTER.AM, LLC
+ * @author    Bogdan Mamontov <bohdan.mamontov@dotsplatform.com>
+ */
+
+namespace Dotsplatform\CashbackApi\DTO\Request\Transactions;
+
+
+use Illuminate\Contracts\Support\Arrayable;
+
+class TransactionsStatisticsDTO implements Arrayable
+{
+    protected function __construct(
+        private float $receivingAmountSum,
+        private float $depositAmountSum,
+        private int $receivingCount,
+        private int $depositCount,
+    ) {
+    }
+
+    public static function fromArray(array $data): static
+    {
+        return new static(
+            $data['receivingAmountSum'] ?? 0,
+            $data['depositAmountSum'] ?? 0,
+            $data['receivingCount'] ?? 0,
+            $data['depositCount'] ?? 0,
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'receivingAmountSum' => $this->getReceivingAmountSum(),
+            'depositAmountSum' => $this->getDepositAmountSum(),
+            'receivingCount' => $this->getReceivingCount(),
+            'depositCount' => $this->getDepositCount(),
+        ];
+    }
+
+    public function getReceivingAmountSum(): float
+    {
+        return $this->receivingAmountSum;
+    }
+
+    public function getDepositAmountSum(): float
+    {
+        return $this->depositAmountSum;
+    }
+
+    public function getReceivingCount(): int
+    {
+        return $this->receivingCount;
+    }
+
+    public function getDepositCount(): int
+    {
+        return $this->depositCount;
+    }
+}
