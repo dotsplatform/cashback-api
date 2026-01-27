322aea15d7006137c69f6d0591f29e9bfd37a566
Bohdan Mamontov <bengemingg@gmail.com>
2026-01-28 01:11:24 +0200

    Update transactions search

--- src/App/DTO/Response/Transactions/SearchTransactionsResponseDTO.php
+++ src/App/DTO/Response/Transactions/SearchTransactionsResponseDTO.php
@@ -17,6 +17,14 @@ class SearchTransactionsResponseDTO extends DTO
 
     protected TransactionsStatisticsDTO $statistics;
 
+    public static function fromArray(array $data): static
+    {
+        $data['items'] = ResponseTransactionsWithOrderAndUser::fromArray($data['items'] ?? []);
+        $data['statistics'] = TransactionsStatisticsDTO::fromArray($data['statistics'] ?? []);
+
+        return parent::fromArray($data);
+    }
+
     public function getItems(): ResponseTransactionsWithOrderAndUser
     {
         return $this->items;
